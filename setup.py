c2fb1f84214e0d35a29819598f4ca5983b34d003
sispoof <mbenhamouda099@gmail.com>
2022-06-06 02:41:45 +0100

    last version

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='sqlite3',
+   version='1.0',
+   description='A useful module',
+   author='Man Foo',
+   author_email='ahmed.benhamouda@sesame.com.tn',
+   packages=['sqlite3'],  #same as name
+)
